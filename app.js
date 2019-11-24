a0ccafa9a2abee823ea6b9544db7356f4a03dd0d
Chinedu Ebinim <chinedu@verifi.ng>
2019-11-24 09:26:34 +0100

    log server start

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const schema = require('./graphql/schema');
-const mongoose = require('mongoose');
-const isAuth = require('./middleware/is-auth');
-const resolver = require('./graphql/resolvers');
+const express = require("express");
+const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const schema = require("./graphql/schema");
+const mongoose = require("mongoose");
+const isAuth = require("./middleware/is-auth");
+const resolver = require("./graphql/resolvers");
 
 const app = express();
-
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin','*');
-    res.setHeader('Access-Control-Allow-Method','POST, GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers','Content-Type, Authorization');
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Method", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
-    if(req.method === "OPTIONS") return res.sendStatus(200);
-    next();
-})
+  if (req.method === "OPTIONS") return res.sendStatus(200);
+  next();
+});
 
 app.use(isAuth);
 
-app.use('/graphql', graphqlHttp({
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: schema,
     rootValue: resolver,
     graphiql: true
-}));
+  })
+);
 
 mongoose
   .connect(`mongodb://127.0.0.1:27017/survey-app`, {
@@ -37,7 +39,7 @@ mongoose
   .then(() => {
     app.listen(8000);
     console.log(`we're live at port:8000`);
-})
-.catch((error) => {
-    console.error(error.message)
-})
+  })
+  .catch(error => {
+    console.error(error.message);
+  });
